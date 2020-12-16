0aa88e0c7142cfabc129e9dc414e96198ff9e064
X-workzDev01 <xworkzdev01@gmail.com>
2020-12-16 15:16:22 +0530

    Added : Apptitudes programme related to diagonals

---
+++ src/com/xworkz/starPatterns/Pattern.java
@@ -0,0 +1,24 @@
+package com.xworkz.patterns;
+
+public class Pattern {
+	
+	public static void main(String[] args) {
+		int num=4;
+		for (int i = 0; i <= num; i++) {
+			for (int j = 0; j <= num; j++) {
+				if (i==j || i+j==num ) {
+					System.out.print("*");
+				}
+				else if (i==0 || j==0 ||j==num |i==num) {
+					System.out.print("@");
+				}
+				else 
+					System.out.print("-");
+			
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
